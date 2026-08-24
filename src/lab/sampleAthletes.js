62c074dd2525e93d4eb304e0d263b4e79c2b97eb
hammer1david <hammer1.david@gmail.com>
2026-08-24 09:37:21 +0200

    Create sampleAthletes.js

---
+++ src/lab/sampleAthletes.js
@@ -0,0 +1,9 @@
+export const sampleAthlete = Object.freeze({
+  goalEvent: '5K',
+  phase: 'Loading',
+  current5k: '15:00',
+  tolerance: 'established',
+  primaryNeed: 'Threshold',
+  readiness: 82,
+  recentWorkout: '5x1000 @3:03 / 60s',
+});
