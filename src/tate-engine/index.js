3f1255f48da801eeb5ff5057288d75a88a679a62
hammer1david <hammer1.david@gmail.com>
2026-08-24 14:11:26 +0200

    Delete src/tate-engine/index.js

--- src/tate-engine/index.js
+++
@@ -1,5 +0,0 @@
-export { ENGINE_VERSION } from './config.js';
-export { parseWorkoutText } from './parser.js';
-export { classifyWorkout } from './classifier.js';
-export { generateCandidates, formatCandidate } from './generator.js';
-export { learnPreference } from './learning.js';
